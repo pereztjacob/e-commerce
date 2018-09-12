364db170fb95eb7561900a1ad5163a8bf6aad144
pereztjacob <pereztjacob@gmail.com>
2018-09-12 16:43:06 -0400

    create files and write store/services files

---
+++ src/frontend/store/promiseMiddleware.js
@@ -0,0 +1,29 @@
+import { LOAD_START, LOAD_END, ERROR } from '../components/app/error-loading/reducers';
+
+const isPromise = val => val && typeof val.then === 'function';
+
+export default ({ dispatch }) => next => action => {
+
+  const { type, payload } = action;
+  if(!isPromise(payload)) return next(action);
+
+  dispatch({ type: LOAD_START });
+
+  return payload
+    .then(
+      result => {
+        dispatch({
+          type: type,
+          payload: result
+        });
+
+        dispatch({ type: LOAD_END });
+        return result;
+      },
+      err => {
+        dispatch({ type: LOAD_END });
+        dispatch({ type: ERROR, payload: err });
+        throw err;
+      }
+    );
+};
